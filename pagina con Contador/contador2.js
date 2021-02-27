26bacbf8f8634d1f1f4ed05daa96bf2f312dfe45
anacamila-hdez <anacamila.hdez@gmail.com>
2021-02-27 18:38:37 -0500

    contador

--- pagina con Contador/contador2.js
+++ pagina con Contador/contador2.js
@@ -2,6 +2,7 @@ let contador = JSON.parse(localStorage.getItem("contador"))
 
 if (contador == null){
     localStorage.setItem("contador",1)
+    alert(`usted ha ingresado 1 vez`)
 } else{
     suma();
 }
@@ -11,6 +12,6 @@ function suma(){
     contador = contador + 1;
     localStorage.setItem("contador", contador);
     console.log(contador);
+    alert(`usted ha ingresado ${contador} veces`)
 }
 
-alert(`usted ha ingresado ${contador} veces`)
